b039435d337e4cddabbfe7eed1ee019c453f8fdb
JulianSchwaiger <Julian.m.schwaiger@gmail.com>
2021-10-13 18:40:54 +0200

    added registry entry for identification code

--- IdeaManager/MainWindow.xaml.cs
+++ IdeaManager/MainWindow.xaml.cs
@@ -20,6 +20,8 @@ namespace IdeaManager
     /// </summary>
     public partial class MainWindow : Window
     {
+
+        private string IDENTIFICATION_CODE = "";
         public MainWindow()
         {
             InitializeComponent();
@@ -29,11 +31,75 @@ namespace IdeaManager
         {
             //Call Backend function for generating a new user identification code.
             //Display it in the txtBox
+
+            //popup asking if user is sure that he wants to create a new code
+            lblNoCodeFound.Visibility = Visibility.Collapsed;
+
+            SetIdentificationCode("testCode");
+            
         }
 
         private void Window_Loaded(object sender, RoutedEventArgs e)
         {
-            
+
+            LoadIdentificationCode();
+            btnDeleteCode.Visibility = Visibility.Visible;
+        }
+
+
+        private void LoadIdentificationCode()
+        {
+            var key = Microsoft.Win32.Registry.CurrentUser.OpenSubKey("IdeaFinder");
+            if (key == null)
+            {
+                key = Microsoft.Win32.Registry.CurrentUser.CreateSubKey("IdeaFinder");
+                SettingsTab.IsSelected = true;
+                lblNoCodeFound.Visibility = Visibility.Visible;
+            }
+            else
+            {
+                var idCode = key.GetValue("IdentificationCode");
+
+                if (idCode == null)
+                {
+                    lblNoCodeFound.Visibility = Visibility.Visible;
+                    txtCode.Text = "";
+                }
+                else
+                {
+                    txtCode.Text = idCode.ToString();
+                    IDENTIFICATION_CODE = idCode.ToString();
+                }
+            }
+            key.Close();
+        }
+
+        private void SetIdentificationCode(string newCode)
+        {
+            var key = Microsoft.Win32.Registry.CurrentUser.OpenSubKey("IdeaFinder", true);
+            if(key != null)
+                key.SetValue("IdentificationCode", newCode);
+
+            LoadIdentificationCode();
+        }
+
+        /// <summary>
+        ///     USED FOR DEBUG PURPOSES ONLY
+        /// </summary>
+        private void DeleteIdentificationCode()
+        {
+            var key = Microsoft.Win32.Registry.CurrentUser.OpenSubKey("IdeaFinder", true);
+            if (key != null) key.DeleteValue("IdentificationCode");
+        }
+
+
+        /// <summary>
+        ///     USED FOR DEBUG PURPOSES ONLY
+        /// </summary>
+        private void btnDeleteCode_Click(object sender, RoutedEventArgs e)
+        {
+            DeleteIdentificationCode();
+            LoadIdentificationCode();
         }
     }
 }
